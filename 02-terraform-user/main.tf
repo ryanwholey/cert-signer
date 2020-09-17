bb352b57cee41085775d201cf7ad34b35444e407
Ryan Wholey <ryan@takescoop.com>
2020-09-17 15:54:01 -0700

    Update readme

--- 02-terraform-user/main.tf
+++ 02-terraform-user/main.tf
@@ -6,48 +6,6 @@ data "vault_policy_document" "terraform" {
     capabilities = ["create", "read", "update", "delete", "list", "sudo"]
     description  = "Allows Terraform to manage paths broadly across Vault."
   }
-
-  # rule {
-  #   path         = "auth/*"
-  #   capabilities = ["create", "update", "delete", "read", "sudo"]
-  #   description  = "Manage authentication backends broadly across Vault"
-  # }
-
-  # rule {
-  #   path         = "sys/auth/*"
-  #   capabilities = ["create", "update", "delete", "sudo"]
-  #   description  = "Create, modify, and delete authentication backends"
-  # }
-
-  # rule {
-  #   path         = "sys/mounts/*"
-  #   capabilities = ["create", "read", "update", "delete", "sudo"]
-  #   description  = "Mount and manage secret backends broadly across Vault"
-  # }
-
-  # rule {
-  #   path         = "auth/token/create"
-  #   capabilities = ["create", "update", "delete", "sudo"]
-  #   description  = "Allow terraform role to create short-lived tokens, used as an automatic process by the Vault provider."
-  # }
-
-  # rule {
-  #   path         = "auth/token/lookup"
-  #   capabilities = ["create", "read", "update", "delete"]
-  #   description  = "Allow terraform role to create short-lived tokens, used as an automatic process by the Vault provider."
-  # }
-
-  # rule {
-  #   path         = "auth/token/lookup-self"
-  #   capabilities = ["read"]
-  #   description  = "Allow terraform role to read information about its token."
-  # }
-
-  # rule {
-  #   path         = "sys/capabilities-self"
-  #   capabilities = ["read", "update"]
-  #   description  = "Allow checking the capabilities of terraform role token."
-  # }
 }
 
 resource "vault_auth_backend" "approle" {
--- reaadme.md
+++ reaadme.md
@@ -1,6 +1,16 @@
 # Vault SSH Certs
 
-Init and apply project `01-*`
+## Setup
+
+This projet uses env vars to set a few variables across the projects. Copy `.env-example` to `.env` and fill out the required fields.
+
+You can add them to your shell by running . ./env
+
+## 01-platform
+
+This project creates the public Vault cluster.
+
+terraform init and apply project `01-*`
 
 Unseal Vault:
 
@@ -17,4 +27,33 @@ Save the root token in your env file
 
 ```sh
 TF_VAR_vault_root_token=<token>
+```
+
+## 02-terraform-user
+
+This project creates the terraform user we will use to provision the third project.
+
+terraform init and apply
+
+
+## 03-instances
+
+This project creates a public bastion asg and an instance in the private subnet to. You can also uncomment the rds code to create a small database.
+
+terraform init and apply
+
+
+## CLI
+
+The logs into Vault, requests a signed client certificate and stores it in ~/.ssh/id_rsa-cert.pub, and requests the host ca public key and stores it ~/.ssh/known-hosts.
+
+### Getting started
+
+```sh
+cd cli
+npm install
+./bin/cli \
+  --role platform \       # platform or engineer, engineer won't be able to reach the private instance
+  --host <host> \         # host or bastion
+  --environment staging   # any env will target the same bastion at the momment
 ```
